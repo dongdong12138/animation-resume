1fda650c6c08aa7711242e382b31e14039791cc4
dongdong <286084845@qq.com>
2018-11-01 13:42:26 +0800

    js文件

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-/* 把code写到#code和style标签里 */
-function writeCode(prefix, code, fn) {
-  let domCode = document.querySelector('#code')
-  let n = 0
-  let id = setInterval(() => {
-    n += 1
-
-    // 以css的语法高亮html中的内容
-    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css, 'css')
-
-    styleTag.innerHTML = prefix + code.substring(0, n)
-    domCode.scrollTop = domCode.scrollHeight // 让 scroll 能拉多长拉多长
-    if (n >= code.length) {
-      window.clearInterval(id)
-      fn.call()
-    }
-  }, 5)
-}
-function writeMarkdown(markdown, fn) {
-  let domPaper = document.querySelector('#paper > .content')
-  let n = 0
-  let id = setInterval(() => {
-    n += 1
-
-    // 以css的语法高亮html中的内容
-    domPaper.innerHTML = (markdown.substring(0, n))
-
-    domPaper.scrollTop = domPaper.scrollHeight // 让 scroll 能拉多长拉多长
-    if (n >= markdown.length) {
-      window.clearInterval(id)
-      fn.call()
-    }
-  }, 5)
-}
-
-
 var result = `/*
  * 面试官您好，我是董董董
  * 我将以动画的形式来介绍我自己
@@ -48,10 +12,14 @@ var result = `/*
   body {
     background: rgb(222, 222, 222);
     font-size: 16px;
+    display: flex;
+    justify-content: space-around;
   }
-  #code {
-    border: 1px solid red;
-    padding: 16px;
+  .code-wrapper {
+    width: 50%;
+    height: 100vh;
+    border: 1px solid #aaa;
+    padding-left: 64px;
   }
 
   /* 我需要一点代码高亮 */
@@ -69,41 +37,58 @@ var result = `/*
   }
 
   /* 加点 3D 效果 */
+  .code-wrapper {
+    perspective: 500;
+    -webkit-perspective: 500; /* Safari 和 Chrome */  
+  }
   #code {
-    transform: rotate(360deg);
+     transform:rotateY(10deg);
   }
 
   /* 不玩了，我来介绍一下我自己吧 */
   /* 我需要一张白纸 */
-  #code {
+  .code-wrapper {
     position: fixed;
     left: 0;
-    width: 50%;
-    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #code {
+    width: 98%;
+    height: 80%;
+    border: 1px solid red;
+    padding-left: 64px;
   }
   #paper {
     position: fixed;
     right: 0;
     width: 50%;
     height: 100%;
-    background: #000;
-    padding: 8px;
+    background: #444;
+    padding: 16px;
     display: flex;
     justify-content: center;
     align-items: center;
+    perspective: 800;
+    -webkit-perspective: 800; /* Safari 和 Chrome */
   }
   #paper > .content {
     background: #fff;
-    width: 100%;
+    width: 80%;
     height: 100%;
+    overflow: auto;
+    padding: 16px;
+    transform:rotateY(-10deg);
   }
+`
+var result2 = `
+  /* 用Markdown打个草稿先 */  
+`
+var result3 = `
+  /* 现在把 Markdown 变成 HTML */
 `
 
-var result2 = `
-  #paper {
-  }
-`
-/* 接下来把 Markdown 变成 HTML */
 
 /* 接下来给 HTML 加样式 */
 
@@ -162,16 +147,64 @@ XXX 学校毕业
 writeCode('', result, () => {
   createPaper(() => {
     writeCode(result, result2, () => {
-      writeMarkdown(md)
+      writeMarkdown(md, () => {
+        writeCode(result + result2, result3, () => {
+          markdownToHtml(md)
+        })
+      })
     })
   })
 })
+
+/* 把code写到#code和style标签里 */
+function writeCode(prefix, code, fn) {
+  let domCode = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+
+    // 以css的语法高亮html中的内容
+    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css, 'css')
+
+    styleTag.innerHTML = prefix + code.substring(0, n)
+    domCode.scrollTop = domCode.scrollHeight // 让 scroll 能拉多长拉多长
+    if (n >= code.length) {
+      window.clearInterval(id)
+      fn.call()
+    }
+  }, 10)
+}
+
+function writeMarkdown(markdown, fn) {
+  let domPaper = document.querySelector('#paper > .content')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+
+    // 以css的语法高亮html中的内容
+    domPaper.innerHTML = (markdown.substring(0, n))
+
+    domPaper.scrollTop = domPaper.scrollHeight // 让 scroll 能拉多长拉多长
+
+    if (n >= markdown.length) {
+      window.clearInterval(id)
+      fn.call()
+    }
+  }, 10)
+}
+
+function markdownToHtml(markdown, fn) {
+  let domPaper = document.querySelector('#paper > .content')
+  // 将 markdown 转为 HTML
+  domPaper.innerHTML = marked(markdown)
+}
 
 function createPaper(fn) {
   var paper = document.createElement('div')
   paper.id = 'paper'
   var content = document.createElement('pre')
   content.className = 'content'
+  content.scrollTop = content.scrollHeight // 让 scroll 能拉多长拉多长
   paper.appendChild(content)
   document.body.appendChild(paper)
   fn.call()
