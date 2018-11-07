31c1cfb1a505553f9772bce7d808c26ab4576992
dongdong <286084845@qq.com>
2018-11-07 13:52:56 +0800

    第一版

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-var result = `/*
- * 面试官您好，我是董董董
- * 我将以动画的形式来介绍我自己
- * 只用文字介绍太单调了
- * 我就用代码来介绍吧
- * 首先准备一些样式
- */
+// var text = `html {color: red;}`
+// var html = Prism.highlight(text, Prism.languages.css)
 
-  * {
-    transition: all 1s;
-  }
-  body {
+var css1 = `
+  /* 
+   * 面试官你好，我是XXX
+   * 只用文字作做我介绍太单调了
+   * 我就用代码来介绍吧
+   */
+
+  /* 首先来准备一些样式 */
+  html {
     background: rgb(222, 222, 222);
     font-size: 16px;
-    display: flex;
-    justify-content: space-around;
   }
-  .code-wrapper {
-    width: 50%;
+  body {
+    width: 100vw;   
     height: 100vh;
-    border: 1px solid #aaa;
-    padding-left: 64px;
+    display: flex;
   }
 
   /* 我需要一点代码高亮 */
@@ -27,7 +24,7 @@ var result = `/*
     color: #690;
   }
   .token.property {
-    color: #905;
+     color: #905;
   }
   .token.function {
     color: #DD4A68;
@@ -36,178 +33,158 @@ var result = `/*
     color: #999;
   }
 
-  /* 加点 3D 效果 */
-  .code-wrapper {
-    perspective: 500;
-    -webkit-perspective: 500; /* Safari 和 Chrome */  
-  }
-  #code {
-     transform:rotateY(10deg);
+  /* 来弄个呼吸灯的效果 */
+  .code {
+    animation: breath 0.5s infinite alternate-reverse;
   }
 
-  /* 不玩了，我来介绍一下我自己吧 */
-  /* 我需要一张白纸 */
-  .code-wrapper {
-    position: fixed;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  #code {
-    width: 98%;
-    height: 80%;
-    border: 1px solid red;
-    padding-left: 64px;
-  }
-  #paper {
-    position: fixed;
-    right: 0;
+  /* 我还需要一张白纸 */
+  .codeWrapper {
     width: 50%;
-    height: 100%;
-    background: #444;
-    padding: 16px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    perspective: 800;
-    -webkit-perspective: 800; /* Safari 和 Chrome */
   }
-  #paper > .content {
-    background: #fff;
-    width: 80%;
-    height: 100%;
-    overflow: auto;
-    padding: 16px;
-    transform:rotateY(-10deg);
+  .paperWrapper {
+      width: 50%;
+      height: 100%;
+      background: #444;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  .paper {
+      width: 96%;
+      height: 96%;
+      background: #fff;    
+      padding: 16px;
   }
 `
-var result2 = `
-  /* 用Markdown打个草稿先 */  
+var css2 = `
+  /* 接下来以 MarkDown 的形式来写一下简历 */
 `
-var result3 = `
-  /* 现在把 Markdown 变成 HTML */
+var css3 = `
+  /* emmmm，MarkDown 格式似乎并不友好 */
+  /* 那就把 MarkDown 格式变成 HTML 格式吧 */
 `
-
-
-/* 接下来给 HTML 加样式 */
-
-/*
- * 这就是我的会动的简历了
- * 谢谢观看
- */
-
 var md = `
-# 自我介绍
-我叫 XXX
-1990 年 1 月出生
-XXX 学校毕业
-自学前端半年
-希望应聘前端开发岗位
-# 技能介绍
-熟悉 JavaScript CSS
-# 项目介绍
-1. XXX 轮播
-2. XXX 简历
-3. XXX 画板
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
+  # 自我介绍
+  我叫 XXX
+  1990 年 1 月出生
+  XXX 学校毕业
+  自学前端半年
+  希望应聘前端开发岗位
+  # 技能介绍
+  熟悉 JavaScript CSS
+  # 项目介绍
+  1. XXX 轮播
+  2. XXX 简历
+  3. XXX 画板
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
+  # 联系方式
+  - QQ xxxxxxxx
+  - Email xxxxxxxx
+  - 手机 xxxxxxx
 `
 
-writeCode('', result, () => {
-  createPaper(() => {
-    writeCode(result, result2, () => {
-      writeMarkdown(md, () => {
-        writeCode(result + result2, result3, () => {
-          markdownToHtml(md)
-        })
-      })
-    })
-  })
-})
+var n = 0
+var id = setInterval(() => {
+  n += 1
+  code.innerHTML = css1.substring(0, n)
+  code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
+  styleTag.innerHTML = css1.substring(0, n)
 
-/* 把code写到#code和style标签里 */
-function writeCode(prefix, code, fn) {
-  let domCode = document.querySelector('#code')
-  let n = 0
-  let id = setInterval(() => {
-    n += 1
+  code.scrollTop = code.scrollHeight
+  if (n >= css1.length) {
+    window.clearInterval(id)
+    fn2()
+    fn3(css1)
+  }
+}, 10)
 
-    // 以css的语法高亮html中的内容
-    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css, 'css')
+function fn2() {
+  // <div class="paperWrapper">
+  //       <pre id="paper" class="paper"></pre>
+  //   </div>
 
-    styleTag.innerHTML = prefix + code.substring(0, n)
-    domCode.scrollTop = domCode.scrollHeight // 让 scroll 能拉多长拉多长
-    if (n >= code.length) {
-      window.clearInterval(id)
-      fn.call()
-    }
-  }, 10)
-}
-
-function writeMarkdown(markdown, fn) {
-  let domPaper = document.querySelector('#paper > .content')
-  let n = 0
-  let id = setInterval(() => {
-    n += 1
-
-    // 以css的语法高亮html中的内容
-    domPaper.innerHTML = (markdown.substring(0, n))
-
-    domPaper.scrollTop = domPaper.scrollHeight // 让 scroll 能拉多长拉多长
-
-    if (n >= markdown.length) {
-      window.clearInterval(id)
-      fn.call()
-    }
-  }, 10)
-}
-
-function markdownToHtml(markdown, fn) {
-  let domPaper = document.querySelector('#paper > .content')
-  // 将 markdown 转为 HTML
-  domPaper.innerHTML = marked(markdown)
-}
-
-function createPaper(fn) {
-  var paper = document.createElement('div')
+  var paperWrapper = document.createElement('div')
+  paperWrapper.className = 'paperWrapper'
+  var paper = document.createElement('pre')
   paper.id = 'paper'
-  var content = document.createElement('pre')
-  content.className = 'content'
-  content.scrollTop = content.scrollHeight // 让 scroll 能拉多长拉多长
-  paper.appendChild(content)
-  document.body.appendChild(paper)
-  fn.call()
+  paper.className = 'paper'
+  paperWrapper.appendChild(paper)
+  document.body.appendChild(paperWrapper)
 }
 
+function fn3(preResult) {
+  var n = 0
+  var id = setInterval(() => {
+    n += 1
+    code.innerHTML = preResult + css2.substring(0, n)
+    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
+    styleTag.innerHTML = preResult + css2.substring(0, n)
 
+    code.scrollTop = code.scrollHeight
+    if (n >= css2.length) {
+      window.clearInterval(id)
+      writeMarkdown()
+    }
+  }, 10)
+}
+
+function writeMarkdown() {
+  var n = 0
+  var id = setInterval(() => {
+    n += 1
+    paper.innerHTML = md.substring(0, n)
+    paper.scrollTop = paper.scrollHeight
+    if (n >= md.length) {
+      window.clearInterval(id)
+      fn4(css1+css2)
+    }
+  }, 10)
+}
+
+function fn4(preResult) {
+  var n = 0
+  var id = setInterval(() => {
+    n += 1
+    code.innerHTML = preResult + css3.substring(0, n)
+    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
+    styleTag.innerHTML = preResult + css3.substring(0, n)
+
+    code.scrollTop = code.scrollHeight
+    if (n >= css3.length) {
+      window.clearInterval(id)
+      markdownToHtml()
+    }
+  }, 10)
+}
+
+function markdownToHtml() {
+  paper.innerHTML = marked(md)
+}
